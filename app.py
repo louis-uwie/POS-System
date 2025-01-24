1c36df1d7601eea5161ad51f0863cdce1e0d8aae
uwie <157711065+louis-uwie@users.noreply.github.com>
2025-01-24 15:35:46 +0800

    Initializing register feature

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, session, url_for
 from flask_session import Session
+from flask_bcrypt import Bcrypt
 
 from datetime import datetime, timedelta
 
@@ -11,6 +12,7 @@ Initializing Application
 This is the basic Flask startup method for the web-app to run.
 """
 app = Flask(__name__)
+bycrypt = Bcrypt(app)
 
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SESSION_PERMANENT'] = False
@@ -33,6 +35,9 @@ The following routes are defined:
 - /login    :   This is the login page. Pre-defined users only. No registration.
                 Only ADMINS have access to adding new users to the system. 
 
+- /register :   This is the registration page. Only ADMINS have access to this page.
+                This is where new employees can be added to the system.
+
 - /main     :   This is the landing page for employees. This is where the main directory will be displayed
                 In main, the employee is able to go through different features of the application.
                 It could be sales, viewing sales history, check product prices, and other basic applications of a POS-System
@@ -42,6 +47,7 @@ The following routes are defined:
 def index():
 
     return render_template('index.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -66,3 +72,20 @@ def login():
 if __name__ == "__main__":
   app.run()
 
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        role = request.form['role']
+
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        cursor.execute("INSERT INTO users (username, password, role) VALUES (?, ?, ?)", (username, password, role))
+        conn.commit()
+
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
